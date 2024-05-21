491e476c94da5f9fe4b37376f0ca8c18dcf8c158
Fernando Vaz <fertwbr@gmail.com>
2024-05-21 18:03:28 -0300

    Refactor: Introduce CepServiceException hierarchy to follow OCP
    - Establishes a hierarchy of exceptions (`CepServiceException` as the base) to make error handling more extensible.

--- src/main/java/io/github/locatecep/service/CepService.java
+++ src/main/java/io/github/locatecep/service/CepService.java
@@ -2,25 +2,28 @@ package io.github.locatecep.service;
 
 import io.github.locatecep.model.Cep;
 import io.github.locatecep.repository.CepRepository;
+import io.github.locatecep.service.exception.CepServiceException;
 import io.github.locatecep.service.exception.NotFoundException;
 
 public class CepService {
 
-    public Cep buscar(String numeracao) {
+    private CepRepository cepRepository;
 
-        if(numeracao.length() > 8) {
-            throw new NotFoundException("Cep inválido.");
+    public CepService(CepRepository cepRepository) {
+        this.cepRepository = cepRepository;
+    }
+
+    public Cep buscar(String numeracao) {
+        if (numeracao.length() > 8) {
+            throw new CepServiceException("CEP inválido.");
         }
 
-        CepRepository repository = new CepRepository();
-        Cep cep = repository.buscarCep(numeracao);
+        Cep cep = cepRepository.buscarCep(numeracao);
 
-        if(cep == null) {
-            throw new NotFoundException("Não foram encontrados registros para o cep informado.");
+        if (cep == null) {
+            throw new NotFoundException("Não foram encontrados registros para o CEP informado.");
         }
 
         return cep;
-
     }
-
 }
---
+++ src/main/java/io/github/locatecep/service/exception/CepServiceException.java
@@ -0,0 +1,7 @@
+package io.github.locatecep.service.exception;
+
+public class CepServiceException extends RuntimeException {
+    public CepServiceException(String message) {
+        super(message);
+    }
+}
