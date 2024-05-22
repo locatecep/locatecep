0a54192e10f3ab4657dc00e3d208a36f67d95287
Beatriz Gibaut <beahhtriz01@gmail.com>
2024-05-22 20:20:25 -0300

    feat: add javadoc documentation, close #3

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"io.github.locatecep.config"},{"l":"io.github.locatecep.model"},{"l":"io.github.locatecep.repository"},{"l":"io.github.locatecep.service"},{"l":"io.github.locatecep.service.exception"},{"l":"io.github.locatecep.view"}];updateSearchResults();
